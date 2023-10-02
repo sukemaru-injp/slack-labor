3b36b4e33ef00f768c38630241843c8e65c9c37f
sukemaru-injp <sukemaro_0@yahoo.co.jp>
2023-10-02 15:47:03 +0900

    時間入力

--- src/main/scala/routes/BlockActions.scala
+++ src/main/scala/routes/BlockActions.scala
@@ -1,5 +1,6 @@
 package routes
 
 object BlockActions {
+  val laborStartStartDateTime = "labor_start_datetime_action"
   val laborMemoActionId = "labor_memo_action"
 }
--- src/main/scala/utils/slack/DiaryModal.scala
+++ src/main/scala/utils/slack/DiaryModal.scala
@@ -2,14 +2,20 @@ package utils.slack
 
 import com.slack.api.model.block.Blocks.{asBlocks, input}
 import com.slack.api.model.block.composition.BlockCompositions.plainText
-import com.slack.api.model.block.element.BlockElements.plainTextInput
+import com.slack.api.model.block.element.BlockElements.{
+  plainTextInput,
+  timePicker
+}
+import com.slack.api.model.block.element.TimePickerElement
 import com.slack.api.model.view.View
 import com.slack.api.model.view.Views.{view, viewClose, viewSubmit, viewTitle}
 import routes.BlockActions
 
 object DiaryModalBlocks {
+  val startDateTime = "start_datetime"
   val memo = "memo"
 }
+
 class DiaryModal(val triggerId: String) extends SlackModal {
   def build(): View = {
     view(
@@ -19,9 +25,18 @@ class DiaryModal(val triggerId: String) extends SlackModal {
         .title(viewTitle(_.`type`("plain_text").text("HelloLabor").emoji(true)))
         .submit(viewSubmit(_.`type`("plain_text").text("Submit").emoji(true)))
         .close(viewClose(_.`type`("plain_text").text("Close")))
-        .blocks(asBlocks(input(_.blockId(DiaryModalBlocks.memo)
-          .element(plainTextInput(_.actionId(BlockActions.laborMemoActionId).multiline(true)))
-          .label(plainText("メモ")))))
+        .blocks(asBlocks(
+          input(
+            _.blockId(DiaryModalBlocks.startDateTime)
+              .element(
+                timePicker(_.actionId(BlockActions.laborStartStartDateTime)))
+              .label(plainText("開始時間"))),
+          input(
+            _.blockId(DiaryModalBlocks.memo)
+              .element(plainTextInput(
+                _.actionId(BlockActions.laborMemoActionId).multiline(true)))
+              .label(plainText("メモ")))
+        ))
     )
   }
 }
