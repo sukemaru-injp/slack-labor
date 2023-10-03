7e5c4e60cb703a37e313820a88ff2595dcdc9639
sukemaru-injp <sukemaro_0@yahoo.co.jp>
2023-10-03 18:19:28 +0900

    map

--- src/main/scala/models/diary/DiaryFactory.scala
+++ src/main/scala/models/diary/DiaryFactory.scala
@@ -1,10 +1,16 @@
 package models.diary
 
 import com.slack.api.bolt.request.builtin.ViewSubmissionRequest
-
+import utils.slack.DiaryModalBlockId
 object DiaryFactory {
   def create(req: ViewSubmissionRequest): Unit = {
-    val stateValues = req.getPayload.getView.getState.getValues
-    println("fireeeee", stateValues)
+    val stateMap = req.getPayload.getView.getState.getValues
+    val memo = stateMap.get(DiaryModalBlockId.memo).values()
+    val startDateTime = stateMap.get(DiaryModalBlockId.startDateTime).values()
+
+    println("fireeeee", memo, startDateTime)
   }
+  private def createMemo() = {}
+  private def createStartDateTime() = {}
+  private def createEndDateTime() = {}
 }
